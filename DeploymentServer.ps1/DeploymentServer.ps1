2edf03225f1816d49eb24e93fa59d731843d84cc
Guillermo TD <guillermo377@gmail.com>
2019-03-21 20:43:06 +0000

    AddedDiskInitialization

---
+++ DeploymentServer.ps1/DeploymentServer.ps1
@@ -0,0 +1,14 @@
+﻿Configuration WindowsDeploymentServices
+{
+    param ($MachineName)
+
+    Node $MachineName
+    { 
+        #Install Windows Deployment Services
+        WindowsFeature WDS
+        {
+          Ensure = "Present"
+          Name = "WDS-Deployment"
+        }
+    }
+}
