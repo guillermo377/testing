cf07b6322e077672be4111103b41a79356296932
Velez <Guillermo.Velez@techdata.com>
2019-03-31 19:47:17 +0200

    ..

---
+++ FolderRights/SetFolderRights.ps1
@@ -0,0 +1,68 @@
+﻿configuration SetFolderRights 
+{ 
+   param 
+   ( 
+
+        [Parameter(Mandatory)]
+        [String]$FirstGroupName,
+        [Parameter(Mandatory)]
+        [String]$SecondGroupName,
+
+        [String]$PathOne = ("H:\$FirstGroupName" + "Data"),
+        [String]$PathTwo = ("I:\$SecondGroupName" + "Data"),
+        [Int]$RetryCount=80,
+        [Int]$RetryIntervalSec=120
+
+    ) 
+    
+    Import-DscResource -ModuleName PSDesiredStateConfiguration,cNtfsAccessControl
+
+    Node localhost
+    {
+     
+        cNtfsPermissionEntry 'FileShareRightsOne'
+
+            {
+                 Ensure = 'Present'
+                 DependsOn = '[xWaitforDisk]Disk2'
+                 Principal = $FirstGroupName +'SecurityGroup'
+                 Path = $PathOne
+                 AccessControlInformation = @(
+
+                       cNtfsAccessControlInformation
+
+                       {
+                            AccessControlType = 'Allow'
+                            FileSystemRights = 'ReadAndExecute'
+                            Inheritance = 'ThisFolderSubfoldersAndFiles'
+                            NoPropagateInherit = $false
+                       }
+                 )
+
+            }
+
+
+        cNtfsPermissionEntry 'FileShareRightsTwo'
+
+            {
+                 Ensure = 'Present'
+                 DependsOn = '[xWaitforDisk]Disk3' 
+                 Principal = $SecondGroupName +'SecurityGroup'
+                 Path = $PathTwo
+                 AccessControlInformation = @(
+
+                       cNtfsAccessControlInformation
+
+                       {
+                            AccessControlType = 'Allow'
+                            FileSystemRights = 'ReadAndExecute'
+                            Inheritance = 'ThisFolderSubfoldersAndFiles'
+                            NoPropagateInherit = $false
+                       }
+                 )
+
+            }
+
+}
+
+} 
